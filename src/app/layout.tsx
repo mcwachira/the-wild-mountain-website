7cc13ebcc71cf34743d5618130c53ab4333acb71
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-08-20 13:08:15 +0300

    Initialized the site using Nextjs and  added some tailwind styles

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import "@/app/styles/globals.css";
+import Header from "@/components/Header";
+import Logo from "@/components/Logo";
+import Navigation from "@/components/Navigation";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+      "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
+
+export default function RootLayout({
+                                     children,
+                                   }: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <html lang="en">
+      <body
+          className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+      <Header />
+
+      <div className="flex-1 px-8 py-12 grid">
+        <main className="max-w-7xl mx-auto w-full">{children}</main>
+      </div>
+      </body>
+      </html>
+  );
+}
