bec4a8981cac244032654a89c0ea21c4cfcd6427
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-05 06:08:07 +0300

    Fetched cabin data and fixed some image issues

--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -1,13 +1,14 @@
 import CabinCard from "@/components/CabinCard";
+import {getCabins} from "@/lib/data-service";
 
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default  async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins =  await getCabins();
 
   return (
     <div>
