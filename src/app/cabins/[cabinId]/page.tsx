942ae59bd45483701e70cdab7279298710588600
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-10 16:28:02 +0300

    highlighted the current link on the side navigation

--- src/app/cabins/[cabinId]/page.tsx
+++ src/app/cabins/[cabinId]/page.tsx
@@ -1,6 +1,7 @@
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import {getCabin, getCabins} from "@/lib/data-service";
 import Image from 'next/image'
+import TextExpander from "@/components/TextExpander";
 
 
 export  async function generateMetaData({params}: {params:{cabinId:string}}) {
@@ -40,7 +41,12 @@ export default async function Page({params}: {params:{cabinId:string}}) {
                         Cabin {name}
                     </h3>
 
-                    <p className="text-lg text-primary-300 mb-10">{description}</p>
+                    <p className="text-lg text-primary-300 mb-10">
+
+                        <TextExpander>
+                            {description}
+                        </TextExpander>
+                  </p>
 
                     <ul className="flex flex-col gap-4 mb-7">
                         <li className="flex gap-3 items-center">
