cca70dfaea3c6c9cb617d0c9a7d7c802b7f2869d
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-11 05:32:21 +0300

    made changes to the individual cabin page and refactored code

--- src/app/cabins/[cabinId]/page.tsx
+++ src/app/cabins/[cabinId]/page.tsx
@@ -1,7 +1,14 @@
+
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
-import {getCabin, getCabins} from "@/lib/data-service";
+import {getBookedDatesByCabinId, getCabin, getCabins, getSettings} from "@/lib/data-service";
 import Image from 'next/image'
 import TextExpander from "@/components/TextExpander";
+import DateSelector from "@/components/DateSelector";
+import ReservationForm from "@/components/ReservationForm";
+import Reservation from "@/components/Reservation";
+import {Suspense} from "react";
+import Spinner from "@/components/Spinner";
+import Cabin from "@/components/Cabin";
 
 
 export  async function generateMetaData({params}: {params:{cabinId:string}}) {
@@ -24,59 +31,30 @@ export async function generateStaticParams() {
     return ids;
 }
 export default async function Page({params}: {params:{cabinId:string}}) {
-    console.log(params.cabinId)
-    const cabin = await getCabin(params.cabinId)
-    const { id, name, maxCapacity, regularPrice, discount, image, description } =
-        cabin;
+    // console.log(params.cabinId)
+
+
+     const cabin = await getCabin(params.cabinId)
+
+    //fetch data in parallel use promise.all to avoid blocking
+
+    //const [cabin, settings, bookedDates] = await Promise.all([ getCabin(params.cabinId), getSettings(),getBookedDatesByCabinId(params.cabinId) ])
+    console.log(cabin)
+
 
     return (
         <div className="max-w-6xl mx-auto mt-8">
-            <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
-                <div className="relative scale-[1.15] -translate-x-3">
-                    <Image src={image} alt={`Cabin ${name}`} fill className="object-cover" />
-                </div>
 
-                <div>
-                    <h3 className="text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">
-                        Cabin {name}
-                    </h3>
-
-                    <p className="text-lg text-primary-300 mb-10">
-
-                        <TextExpander>
-                            {description}
-                        </TextExpander>
-                  </p>
-
-                    <ul className="flex flex-col gap-4 mb-7">
-                        <li className="flex gap-3 items-center">
-                            <UsersIcon className="h-5 w-5 text-primary-600" />
-                            <span className="text-lg">
-                For up to <span className="font-bold">{maxCapacity}</span>{" "}
-                                guests
-              </span>
-                        </li>
-                        <li className="flex gap-3 items-center">
-                            <MapPinIcon className="h-5 w-5 text-primary-600" />
-                            <span className="text-lg">
-                Located in the heart of the{" "}
-                                <span className="font-bold">Dolomites</span> (Italy)
-              </span>
-                        </li>
-                        <li className="flex gap-3 items-center">
-                            <EyeSlashIcon className="h-5 w-5 text-primary-600" />
-                            <span className="text-lg">
-                Privacy <span className="font-bold">100%</span> guaranteed
-              </span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
+            <Cabin cabin={cabin}/>
             <div>
-                <h2 className="text-5xl font-semibold text-center">
-                    Reserve today. Pay on arrival.
+                <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+                Reserve {cabin.name} today. Pay on arrival.
                 </h2>
+
+                <Suspense fallback={<Spinner/>}>
+                    <Reservation cabin={cabin}/>
+                </Suspense>
+
             </div>
         </div>
     );
