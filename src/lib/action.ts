a67d991a5ca9cc32f07ad53d893b26e2baf198f7
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-18 09:08:16 +0300

    Enabled deletion of a reserved booking

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -3,6 +3,8 @@
 import {auth, signIn, signOut} from "@/lib/auth"
 import {supabase} from "@/lib/supabase";
 import {revalidatePath} from "next/cache";
+import {getBookings} from "@/lib/data-service";
+import {isPast} from "date-fns";
 
 export const signInAction  = async () => {
 
@@ -38,4 +40,25 @@ export  async function updateGuest(formData){
     revalidatePath("/account/profile")
     // return data;
 
+}
+export async function deleteReservation(bookingId){
+    const session = await auth();
+    if(!session) throw new Error('You must be logged in')
+
+    const guestBookings = await getBookings(session?.user.guestId)
+    const guestBookingIds = guestBookings.map((booking) => booking.id)
+    if(guestBookingIds.includes(bookingId)) throw new Error('Your not allowed to delete this booking')
+
+       const { data, error } = await supabase.from('bookings').delete().eq('id', bookingId);
+
+   if (error) {
+     console.error(error);
+     throw new Error('Booking could not be deleted');
+   }
+
+   revalidatePath("/account/reservations")
+
+    //
+    // const reservationsToDelete = reservations.filter((reservation) =>reservation.startDate < isPast(new Date(reservation.startDate)) )
+    // console.log(reservationsToDelete)
 }
