5c1637312f04f22e36bf306d163b4a156a87e0fa
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-18 15:02:01 +0300

    Updated the date Selector to prevent picking of booked dates

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { isWithinInterval } from "date-fns";
+import {differenceInDays, isPast, isSameDay, isWithinInterval} from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import {useState} from "react";
@@ -20,11 +20,12 @@ function DateSelector({settings, bookedDates,cabin }) {
 
 
   const {range, setRange, resetRange} = useReservation()
+
+  const displayRange = isAlreadyBooked(range, bookedDates) ?{}:range;
   // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+  const {regularPrice, discount} = cabin;
+  const numNights = differenceInDays(range.to, range.from);
+  const cabinPrice = numNights * (regularPrice  - discount);
   // const range = { from: null, to: null };
 
   // SETTINGS
@@ -36,6 +37,7 @@ function DateSelector({settings, bookedDates,cabin }) {
         className="pt-12 place-self-center"
         mode="range"
         onSelect={setRange}
+        selected={displayRange}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
@@ -43,6 +45,7 @@ function DateSelector({settings, bookedDates,cabin }) {
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(curDate) => isPast(curDate)  || bookedDates.some((date) => isSameDay(date, curDate))}
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
